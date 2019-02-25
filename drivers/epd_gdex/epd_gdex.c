78aa3b54abcb4200c341e95ce5ec522e7892471b
Philipp Blum <philipp-blum@jakiku.de>
2019-02-25 05:56:05 +0100

    Implement init for epd_gdex driver

--- drivers/epd_gdex/epd_gdex.c
+++ drivers/epd_gdex/epd_gdex.c
@@ -1,13 +1,13 @@
 #include <stdint.h>
 #include "periph/gpio.h"
 #include "periph/spi.h"
+#include "xtimer.h"
 #include "epd_gdex_commands.c"
 
 typedef struct {
     uint8_t SPI_BUS = 1;
     uint8_t CS_PIN = GPIO_PIN(0, 0);
 
-    uint8_t BUS_SELECTION_PIN = GPIO_PIN(0, 0);
     uint8_t COMMAND_CONTROL_PIN = GPIO_PIN(0, 0);
     uint8_t RESET_PIN = GPIO_PIN(0, 0);
     uint8_t BUSY_PIN = GPIO_PIN(0, 0);
@@ -33,9 +33,83 @@ epd_gdex_configuration config = {};
 spi_mode_t SPI_MODE = SPI_MODE_0;
 spi_clk_t SPI_CLOCK_SPEED = SPI_CLK_1MHZ;
 
-void epd_gdex_init(epd_gdex_configuration configuration){
-    config = configuration;
+void epd_gdex_command(epd_gdex_command * gdex_command, uint8_t * data, uint8_t data_length){
     spi_init_pins(config.SPI_BUS);
+    for(int i = 0; i < sizeof(gdex_command); i++){
+        if(gdex_command->command){
+            gpio_set(config.COMMAND_CONTROL_PIN);
+        }else{
+            gpio_clear(config.COMMAND_CONTROL_PIN);
+        }
+        spi_transfer_bytes(
+                config.SPI_BUS, config.CS_PIN, true, gdex_command->command, NULL, sizeof(gdex_command->command)
+                );
+    }
+
+    if(data_length > 0){
+        gpio_set(config.COMMAND_CONTROL_PIN);
+        spi_transfer_bytes(config.SPI_BUS, config.CS_PIN, false, data, NULL, data_length);
+        gpio_clear(config.COMMAND_CONTROL_PIN);
+    }
+}
+
+void wait_until_idle(){
+    while(gpio_read (config.BUSY_PIN) == 0){
+        xtimer_usec_from_ticks(100000);
+    }
+}
+
+uint8_t epd_gdex_get_v_resolution(epd_gdex_resolution resolution){
+    switch(config.resolution){
+        case res_96x230:
+            return 96;
+        case res_96x252:
+            return 96;
+        case res_128x296:
+            return 128;
+        case res_160x296:
+            return 160;
+        default:
+            return 96;
+    }
+}
+
+uint16_t epd_gdex_get_h_resolution(epd_gdex_resolution resolution){
+    switch(config.resolution){
+        case res_96x230:
+            return 230;
+        case res_96x252:
+            return 252;
+        case res_128x296:
+            return 296;
+        case res_160x296:
+            return 296;
+        default:
+            return 230;
+    }
+}
+
+void epd_gdex_set_resolution(epd_gdex_resolution resolution){
+    uint8_t v_res_data = (uint8_t) epd_gdex_get_v_resolution(resolution) >> 3;
+    uint8_t h_res_data_upper = (uint8_t)(epd_gdex_get_h_resolution(resolution) >> 8);
+    uint8_t h_res_data_lower = (uint8_t) epd_gdex_get_h_resolution(resolution);
+
+    epd_gdex_command(TRES_SETTINGS, { v_res_data, h_res_data_upper, h_res_data_lower }, 3);
+}
+
+int epd_gdex_init(epd_gdex_configuration configuration){
+    config = configuration;
+
+    spi_init(config.SPI_BUS);
+    epd_gdex_command(BTST_START, { 0x17, 0x17, 0x17 }, 3);
+    epd_gdex_command(POWER_ON, NULL, 0);
+    wait_until_idle();
+    epd_gdex_command(POWER_SETTING, { 0x8F }, 1);
+    epd_gdex_command(VCOM_AND_DATA_INTERVAL_SETTING, { 0x77 }, 1);
+    epd_gdex_set_resolution(config.resolution);
+    epd_gdex_command(VCM_DC_SETTING, { 0X0A }, 1);
+
+    return 0;
 }
 
 void epd_gdex_power_on(){
--- drivers/epd_gdex/epd_gdex_commands.c
+++ drivers/epd_gdex/epd_gdex_commands.c
@@ -1,6 +1,6 @@
 typedef struct {
-    uint8_t command;
-    uint8_t read;
+    bool command;
+    bool read;
     uint8_t data;
 } epd_gdex_command;
 
@@ -140,56 +140,62 @@ typedef enum {
 } epd_gdex_power_selection;
 
 
-epd_gdex_command POWER_SETTING_PSR_START[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x00 }
-};
-
-epd_gdex_command POWER_SETTING_PWR_START[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x00 }
+epd_gdex_command POWER_SETTING[1] = {
+        { .command = false, .read = false, .data = 0x00 }
 };
 
 epd_gdex_command POWER_ON[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x04 }
+        { .command = false, .read = false, .data = 0x04 }
 };
 
 epd_gdex_command POWER_OFF[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x02 }
+        { .command = false, .read = false, .data = 0x02 }
 };
 
 epd_gdex_command DEEP_SLEEP[2] = {
-        { .command = 0x00, .read = 0x00, .data = 0x07 },
-        { .command = 0x00, .read = 0x00, .data = 0xA5 }
+        { .command = false, .read = false, .data = 0x07 },
+        { .command = false, .read = false, .data = 0xA5 }
 };
 
 epd_gdex_command DISPLAY_REFRESH[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x12 }
+        { .command = false, .read = false, .data = 0x12 }
 };
 
 /*
  * BTST = Booster Soft Start
  */
 epd_gdex_command BTST_START[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x06 }
+        { .command = false, .read = false, .data = 0x06 }
 };
 
 epd_gdex_command WHITE_BLACK_START[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x10 }
+        { .command = false, .read = false, .data = 0x10 }
 };
 
 epd_gdex_command SET_COLOR_START[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x13 }
+        { .command = false, .read = false, .data = 0x13 }
 };
 
 epd_gdex_command DATA_STOP[2] = {
-        { .command = 0x00, .read = 0x00, .data = 0x11 }
+        { .command = false, .read = false, .data = 0x11 }
 };
 
 epd_gdex_command TEMPERATURE_SENSOR_CALIBRATION_START[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x40 }
+        { .command = false, .read = false, .data = 0x40 }
 };
 
 epd_gdex_command TEMPERATURE_SENSOR_SELECTION_START[1] = {
-        { .command = 0x00, .read = 0x00, .data = 0x41 }
+        { .command = false, .read = false, .data = 0x41 }
 };
 
+epd_gdex_command VCOM_AND_DATA_INTERVAL_SETTING[1] = {
+        { .command = false, .read = false, .data = 0x50 }
+};
 
+epd_gdex_command TRES_SETTINGS[1] = {
+        { .command = false, .read = false, .data = 0x61 }
+};
+
+epd_gdex_command VCM_DC_SETTING[1] = {
+        { .command = false, .read = false, .data = 0x82 }
+};
