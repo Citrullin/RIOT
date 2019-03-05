38622f15e2c473d7aaadf256cee987d5ed3f8e67
Philipp Blum <philipp-blum@jakiku.de>
2019-03-05 05:13:23 +0100

    Add mesh h and c files to nimble package

---
+++ pkg/nimble/nimble.host.mesh.mk
@@ -0,0 +1,26 @@
+MODULE = nimble_mesh
+
+SRC += access.c
+SRC += adv.c
+SRC += beacon.c
+SRC += cfg_cli.c
+SRC += cfg_srv.c
+SRC += crypto.c
+SRC += friend.c
+SRC += glue.c
+SRC += health_cli.c
+SRC += health_srv.c
+SRC += light_model.c
+SRC += lpn.c
+SRC += mesh.c
+SRC += model_cli.c
+SRC += model_srv.c
+SRC += net.c
+SRC += prov.c
+SRC += proxy.c
+SRC += settings.c
+SRC += shell.c
+SRC += testing.c
+SRC += transport.c
+
+include $(RIOTBASE)/Makefile.base
