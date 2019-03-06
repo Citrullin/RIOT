94f9abb8b4ab5368fcd864eeb766e8b0c7e4d82b
Philipp Blum <philipp-blum@jakiku.de>
2019-03-06 11:55:19 +0100

    Some gpio_util improvements

--- drivers/periph_common/gpio_util.c
+++ drivers/periph_common/gpio_util.c
@@ -8,10 +8,12 @@
 
 #include "periph/gpio.h"
 
+#ifdef MODULE_PERIPH_GPIO
+
 uint8_t gpio_util_shiftin(gpio_t data_pin, gpio_t clock_pin){
     uint8_t byte = 0x00;
 
-    for(unsigned int i = 8; i > 0; i--){
+    for(unsigned int i = 8; i > 0; i--) {
         gpio_set(clock_pin);
         if(gpio_read(data_pin) > 0){
             byte |= (1 << (i - 1));
@@ -21,3 +23,5 @@ uint8_t gpio_util_shiftin(gpio_t data_pin, gpio_t clock_pin){
 
     return byte;
 }
+#endif
+/*MODULE_PERIPH_GPIO*/
